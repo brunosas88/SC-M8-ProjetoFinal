a9f744acb055ef0a5bd973801a63ef887173387e
brunosas88 <bruno.sas@live.com>
2022-03-07 11:42:01 -0300

    Estrutura inicial com gradle

---
+++ src/test/java/br/com/letscode/javaweb/projetofinal/ProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.javaweb.projetofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
